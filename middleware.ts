59cc7625b95261baea860936ad7855f390708852
jdelrio12 <JUSTIN@FORTIVAUSA.COM>
2026-05-28 01:18:16 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,18 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-export default authMiddleware({
-  publicRoutes: ['/sign-in', '/sign-up'],
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    const authObject = await auth()
+    if (!authObject.userId) {
+      return NextResponse.redirect(new URL('/sign-in', req.url))
+    }
+  }
+  return NextResponse.next()
 })
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)'],
 }
