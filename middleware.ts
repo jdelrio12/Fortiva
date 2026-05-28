48d3f1547c86bf830403eabb88653152091b9cf2
jdelrio12 <JUSTIN@FORTIVAUSA.COM>
2026-05-28 13:39:33 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth()
-  if (!userId && !isPublicRoute(req)) {
-    return redirectToSignIn()
+  if (!isPublicRoute(req)) {
+    const { userId } = await auth()
+    if (!userId) {
+      const signInUrl = new URL('/sign-in', req.url)
+      signInUrl.searchParams.set('redirect_url', req.url)
+      return NextResponse.redirect(signInUrl)
+    }
   }
 })
 
